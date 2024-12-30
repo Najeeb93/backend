745954658fe7e30a7cab23ac414206d46f3d530a
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-30 17:07:40 +0500

    add to commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,15 @@ const [jokes, setJokes] = useState([]);
   return (
     <>
     <h1>Full stack</h1>
+    <p>JOKES: {jokes.length}</p>
+    {
+      jokes.map((joke, index) => {
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      })
+    }
     </>
   )
 }
