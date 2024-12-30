05ef525c8062dbe19a1aa05721cec91e65adbdaa
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-30 16:03:02 +0500

    update server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,36 @@ app.get('/', (req, res) => {
     res.send('Server is ready')
 });
 
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A joke',
+            content: 'This is a joke'
+        },
+        {
+            id: 2,
+            title: 'Another joke',
+            content: 'This is another joke'
+        },
+        {
+            id: 3,
+            title: 'A third joke',
+            content: 'This is a third joke'
+        },
+        {
+            id: 4,
+            title: 'A fourth joke',
+            content: 'The is a fourth joke'
+        },
+        {
+            id: 5,
+            title: 'A fifth joke',
+            content: 'This is a fifth joke'
+        }
+    ];
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
