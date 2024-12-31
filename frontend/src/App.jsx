a94d8c12b3dfc9bc62e4e3c02cdf4603fb7cecf0
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-31 18:06:47 +0500

    add to commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,12 +23,12 @@ useEffect(() => {
     <h1>Full stack</h1>
     <p>JOKES: {jokes.length}</p>
     {
-      jokes.map((joke, index) => {
+      jokes.map((joke, index) => (
         <div key={joke.id}>
           <h3>{joke.title}</h3>
           <p>{joke.content}</p>
         </div>
-      })
+      ))
     }
     </>
   )
